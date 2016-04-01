3518bb6f06a200a09e6afe987341c7a952ed8213
magixsource <magixsource.cn@gmail.com>
2016-04-01 14:30:36 +0800

    [Mod] add function parsetToMap

--- src/main/java/robott/dy/protocol/clause/DouyuEncoderObject.kt
+++ src/main/java/robott/dy/protocol/clause/DouyuEncoderObject.kt
@@ -1,8 +1,10 @@
 package robott.dy.protocol.clause
 
+import org.apache.commons.lang3.StringUtils
 import robott.dy.utils.FormatTransfer
 import java.io.ByteArrayOutputStream
 import java.io.DataOutputStream
+import java.util.*
 
 /**
  * Socket data encoder object
@@ -21,7 +23,7 @@ object DouyuEncoderObject {
         return getByte(buf.toString())
     }
 
-    public fun convert(buffer: StringBuffer, Entry: Map.Entry<String, Any>) {
+    fun convert(buffer: StringBuffer, Entry: Map.Entry<String, Any>) {
         buffer?.append(Entry.key.replace("/".toRegex(), "@S").replace("@".toRegex(), "@A"))
         buffer?.append("@=")
         if (Entry.value is String) {
@@ -45,6 +47,33 @@ object DouyuEncoderObject {
         doutput.writeBytes(data)
 
         return boutput.toByteArray()
+    }
+
+    fun parsetToMap(data: String?): Map<String, Any> {
+        val rtnMsg = HashMap<String, Any>()
+
+        //处理数据字符串末尾的'/0字符'
+        val tempData = StringUtils.substringBeforeLast(data, "/")
+
+        //对数据字符串进行拆分
+        val buff = tempData.split("/".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
+
+        //分析协议字段中的key和value值
+        for (tmp in buff) {
+            //获取key值
+            val key = StringUtils.substringBefore(tmp, "@=")
+            //获取对应的value值
+            var value: Any = StringUtils.substringAfter(tmp, "@=")
+
+            //如果value值中包含子序列化值，则进行递归分析
+            if (StringUtils.contains(value as String, "@A")) {
+                value = value.replace("@S".toRegex(), "/").replace("@A".toRegex(), "@")
+                value = parsetToMap(value)
+            }
+            //将分析后的键值对添加到信息列表中
+            rtnMsg.put(key, value)
+        }
+        return rtnMsg
     }
 
 }
