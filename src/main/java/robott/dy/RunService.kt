eeb30a46845f0e1423d13aa1aaf653d9ac658e3a
magixsource <magixsource.cn@gmail.com>
2016-04-03 18:10:59 +0800

    [Mod] modify to multi-thread support

--- src/main/java/robott/dy/RunService.kt
+++ src/main/java/robott/dy/RunService.kt
@@ -8,16 +8,34 @@ import robott.dy.service.PullMessageService
  * Run service script
  * Created by linpeng on 2016/3/31.
  */
-private val roomId = 56040 //138286
+//private val roomId = 14163//56040 //138286
 
 //弹幕池分组号，海量模式使用-9999
 private val groupId = -9999
 
 fun main(args: Array<String>) {
+    // get live rooms
+    val rooms = DouyuService().liveRooms()
+
+    // per room per THREAD to fetch bullet screen
+    for (i in 1..5) {
+        val room = rooms.get(i)
+        val room_id = room.room_id
+        val room_name = room.room_name
+
+        Thread(Runnable {
+            val douyuService = DouyuService()
+            douyuService.connect(room_id, groupId)
+            HeartbeatService(douyuService).start()
+            PullMessageService(douyuService).start()
+        }).start();
+
+    }
+
     // socket connect and data prepare
-    DouyuService.connect(roomId, groupId)
+    //    DouyuService.connect(roomId, groupId)
     // keep alive
-    HeartbeatService.start()
+    //    HeartbeatService.start()
     // pull message
-    PullMessageService.start()
+    //    PullMessageService.start()
 }
